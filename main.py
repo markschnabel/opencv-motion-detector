da3429a2645bb50ef033b0c13ac20cb36cc654fc
Mark Schnabel <markschnabelcs@gmail.com>
2019-01-19 10:24:09 -0500

    Better command line arg options

--- main.py
+++ main.py
@@ -8,19 +8,22 @@ import cv2
 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-ma", "--min-area", type=int, default=500, 
+ap.add_argument("-d", "--delay", type=float, 
+        help="Amount of time in seconds to delay the program before starting.")
+ap.add_argument("-m", "--min-area", type=int, default=500, 
         help="Minimum area in pixels difference to be considered actual motion.")
-ap.add_argument("-vp", "--video-path", 
+ap.add_argument("-t", "--thresh", default=25, type=int,
+        help="Level of threshold intensity.")
+ap.add_argument("-v", "--video-path", 
         help="Path to video file. If not provided the default video recording device on your system will be used.")
 args = vars(ap.parse_args())
 
-print("Program starting.")
-print()
+print("Program starting.\n")
 
-if args.get("video-path", None) is not None:
+if args.get("video_path", None) is not None:
     try:
-        print("Attempting to access the video at path: {}".format(args["video-path"]))
-        video_stream = cv2.VideoCapture(args["video-path"])
+        print("Attempting to access the video at path: {}".format(args["video_path"]))
+        video_stream = cv2.VideoCapture(args["video_path"])
         print("Successfully accessed video.")
     except:
         print("Could not access the specified video. Please make sure you are "
@@ -34,13 +37,20 @@ else:
     except:
         print("Could not access the default recording device. Please make sure "
         + "you have a device capable of recording video configured on your system.")
-    
 print()
+
+if args.get("delay", None) is not None:
+    print("Starting delay of: {} seconds".format(args["delay"]))
+    time.sleep(args["delay"])
+    print("Delay complete.")
 
 # Init variable to hold first frame of video. This will be used as a reference.
 # The motion detection algorithm utilizes the background of the initial frame
 # to compare all consecutive frames to in order to detect motion
 initial_frame = None
+
+print("Starting motion detection")
+print("Enter 'q' at any time to terminate")
 
 # Main loop.
 while True:
@@ -49,7 +59,7 @@ while True:
 
     # Grab current frame
     frame = video_stream.read()
-    frame = frame if args.get("video-path", None) is None else frame[1]
+    frame = frame if args.get("video_path", None) is None else frame[1]
 
     # If frame is none we have reached the end of the video
     if frame is None:
@@ -57,7 +67,7 @@ while True:
     
     # Preprocess frame:
     # Resize to have a width of 500px. Improves speed without sacrificing accuracy
-    frame = imutils.resize(frame, width=500)
+    frame = imutils.resize(frame, width=600)
     # Convert to grayscale as the background subtraction algorithm utilizes 
     # black & white pixel data
     grayscale_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -73,7 +83,7 @@ while True:
     # frame (aka the initial frame)
     frame_delta = cv2.absdiff(initial_frame, grayscale_frame)
     # Compute the threshold of the frame delta
-    thresholded_frame = cv2.threshold(frame_delta, 25, 255, cv2.THRESH_BINARY)[1]
+    thresholded_frame = cv2.threshold(frame_delta, args["thresh"], 255, cv2.THRESH_BINARY)[1]
     # Dialiate the threshold to fill in any gaps/holes that were created.
     dilated_frame = cv2.dilate(thresholded_frame, None, iterations=2)
 
@@ -102,6 +112,10 @@ while True:
 
         status = 'Motion Detected!'
     
+    # If area is vacant set font color to green, else set font color to red
+    # Uses BGR color mapping
+    font_color = (0, 255, 0) if status == 'Area vacant.' else (0, 0, 255)
+
     # Overlay room status onto image
     cv2.putText(
         frame, # Image to overlay text on
@@ -109,32 +123,22 @@ while True:
         (10, 20), # origin of text
         cv2.FONT_HERSHEY_SIMPLEX, # font face
         0.5, # font scale
-        (0, 0, 255), # font color - rgb
+        font_color,
         2 # thickness
     )
 
-    # Overlay cur date & time on image
-    cv2.putText(
-        frame, # Image to overlay date on
-        datetime.datetime.now().strftime("%A %d %B %Y %I:%M:%S%p"), # Date & time
-        (10, frame.shape[0] - 10), # origin of text
-        cv2.FONT_HERSHEY_SIMPLEX, # font face
-        0.35, # font scale
-        (50, 50, 255), # font color - rgb
-        1 # thickness
-    )
-
-    cv2.imshow("Main Feed", frame)
     cv2.imshow("Threshold", dilated_frame)
     cv2.imshow("Frame Delta", frame_delta)
+    cv2.imshow("Main Feed", frame)
 
     key = cv2.waitKey(1) & 0xFF
     if key == ord('q'):
         break 
     
-if args.get("video-path", None) is None:
+if args.get("video_path", None) is not None:
     video_stream.release()
 else:
     video_stream.stop()
 
 cv2.destroyAllWindows()
+print("Program terminating")
